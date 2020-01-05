c5df49760c1b7890a6b4cdc70979f9223c43a9ae
Roman Osadchuk <roman.osadchuk@nerdpeople.com>
2020-01-05 17:52:58 +0200

    - initial commit      (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧

---
+++ GameStarDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameStarDB
+//
+//  Created by Roman Osadchuk on 05.01.2020.
+//  Copyright © 2020 Roman Osadchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
